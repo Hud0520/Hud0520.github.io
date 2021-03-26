2307660ac4245d6ca996c32e7e8e34e354fcd2ef
Hud0520 <hungduong000@gmail.com>
2021-03-26 10:21:59 +0700

    master
    v1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// navbar background color change on scroll
+
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll <= 0){
+            $('.fixed-top').css('background', 'rgba(255, 255, 255, 0.8)');
+            $('.icon-box').removeClass('mt--3');
+            $('.icon-box').addClass('mt-3');
+        } else{
+            $('.fixed-top').css('background', 'white');
+            $('.icon-box').addClass('mt--3');
+            $('.icon-box').removeClass('mt-3');
+        }
+        
+    });
