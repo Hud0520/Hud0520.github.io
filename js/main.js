1b053b4838cefb7813210efe2a0a89eab107f5ca
Hud0520 <hungduong000@gmail.com>
2021-03-29 17:37:29 +0700

    Cạc cạc

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
-        if(scroll <= 0){
+        if(scroll <= 5){
             $('.fixed-top').css('background', 'rgba(255, 255, 255, 0.8)');
             $('.icon-box').removeClass('mt--3');
             $('.icon-box').addClass('mt-3');
