4bcc03057e529a64a3d24449791a289704fa3d58
Hud0520 <hungduong000@gmail.com>
2021-07-30 22:12:04 +0700

    V2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 // navbar background color change on scroll
 
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if(scroll <= 75){
-            $('.fixed-top').css('background', 'rgba(255, 255, 255, 0.8)');
-            $('.icon-box').removeClass('mt--3');
-            $('.icon-box').addClass('mt-3');
-            $('.icon-box').css('opacity','0');
-        } else{
-            $('.fixed-top').css('background', 'white');
-            $('.icon-box').addClass('mt--3');
-            $('.icon-box').removeClass('mt-3');
-            $('.icon-box').css('opacity','1');
-        }
-        
-    });
+$(window).scroll(function(){
+   
+    if($(window).scrollTop()>79){
+        $(".top").addClass("top-scrolled");
+        $("#header").addClass("header-scrolled");
+        $('.booking').addClass('mt--3');
+        $('.booking').removeClass('mt-3');
+    }else{
+        $(".top").removeClass("top-scrolled");
+        $("#header").removeClass("header-scrolled");
+
+        $('.booking').removeClass('mt--3');
+        $('.booking').addClass('mt-3');
+    }
+})
+
+/*Search*/
+$("#show").click(function showSearch(){
+    var indexSeach =$(".booking").position().top;// Lấy vị trí của nó cho vui
+    var iSearch = $(".booking").height()+0.1*screen.height;
+    $("body").animate({scrollTop: iSearch},'slow');
+    //Lấy kích thước ô tìm kiếm + 0,1 chiều cao màn hình
+});
